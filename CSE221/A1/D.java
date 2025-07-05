de6e9253c36e4259ffd3e33f6674d0c80598af3f
Ilham Morsalin <ilhammorsalin@gmail.com>
2025-07-06 00:04:31 +0600

    D done

--- CSE221/A1/D.java
+++ CSE221/A1/D.java
@@ -1,3 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class D {
-    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            int n = Integer.parseInt(br.readLine());
+
+            for (int i = 0; i < n; i++) {
+                int N = Integer.parseInt(br.readLine());
+                String[] str = br.readLine().split(" ");
+
+                boolean isSorted = true;
+
+                for (int j = 1; j < N; j++) {
+                    if (Integer.parseInt(str[j]) < Integer.parseInt(str[j - 1])) {
+                        isSorted = false;
+                        break;
+                    }
+                }
+
+                if (isSorted) {
+                    System.out.println("YES");
+                } else {
+                    System.out.println("NO");
+                }
+            }
+
+        } catch (NumberFormatException e) {
+            System.out.println("Error: Please enter a valid integer.");
+        } finally {
+            br.close();
+        }
+    }
 }
---
+++ CSE221/A1/E.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class E {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+
+            int n = Integer.parseInt(br.readLine());
+            
+
+        } catch (NumberFormatException e) {
+            System.out.println("Error: Please enter a valid integer.");
+        } finally {
+            br.close();
+        }
+    }
+}
