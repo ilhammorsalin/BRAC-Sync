fb76b9b38f51b596be5ba8a1f184aa43efc9b5f4
ilham.morsalin <ilhammorsalin@gmail.com>
2025-07-19 17:48:17 +0600

    A3 - F

--- CSE221/A3/B.java
+++ CSE221/A3/B.java
@@ -6,75 +6,30 @@ public class B {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
-        String[] str = br.readLine().split(" ");
-
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(str[i]);
-        }
-
-        int count = mergeSort(arr);
-
-        System.out.println(count);
-
-        for (int i = 0; i < n; i++) {
-            System.out.print(arr[i] + " ");
-        }
-    }
-
-    public static int mergeSort(int[] arr) {
-        int n = arr.length;
-        int mid = n / 2;
-
-        if (n <= 1) {
-            return 0;
-        }
-
-        int[] left = new int[mid];
-        int[] right = new int[n - mid];
-
-        for (int i = 0; i < mid; i++) {
-            left[i] = arr[i];
-        }
-        for (int i = mid; i < n; i++) {
-            right[i - mid] = arr[i];
-        }
-
-        int countL = mergeSort(left);
-        int countR = mergeSort(right);
-
-        int countMerge = merge(arr, left, right);
-
-        return countL + countR + countMerge;
-    }
-
-    public static int merge(int[] A, int[] left, int[] right) {
-        int count = 0;
-
-        int sizeL = left.length;
-        int sizeR = right.length;
-
-        int i = 0, j = 0, k = 0;
-
-        while (j < sizeL && k < sizeR) {
-            if (left[j] <= right[k]) {
-                A[i++] = left[j++];
-            } else if (left[j] > (right[k]) * (right[k])) {
-                A[i++] = right[k++];
-                count++;
-            } else {
-                A[i++] = right[k++];
+        try {
+            int n = Integer.parseInt(br.readLine());
+            String[] str = br.readLine().split(" ");
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(str[i]);
             }
-        }
 
-        while (j < sizeL) {
-            A[i++] = left[j++];
-        }
-        while (k < sizeR) {
-            A[i++] = right[k++];
-        }
+            int count = 0;
+            for (int i = 0; i < n; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    long sq = (long) arr[j] * (long) arr[j];
+                    if (arr[i] > sq) {
+                        count++;
+                    }
+                }
+            }
 
-        return count;
+            System.out.println(count);
+
+        } catch (NumberFormatException e) {
+            System.out.println("Error: Please enter a valid integer.");
+        } finally {
+            br.close();
+        }
     }
 }
--- CSE221/A3/F.java
+++ CSE221/A3/F.java
@@ -1,5 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class F {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        String[] str = br.readLine().split(" ");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(str[i]);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        printMid(arr, 0, n - 1, sb);
+        System.out.println(sb.toString().trim());
+    }
+
+    public static void printMid(int[] arr, int l, int r, StringBuilder sb) {
+        if (l > r)
+            return;
+        int mid = (l + r) / 2;
+        sb.append(arr[mid]).append(" ");
+        printMid(arr, l, mid - 1, sb);
+        printMid(arr, mid + 1, r, sb);
     }
 }
